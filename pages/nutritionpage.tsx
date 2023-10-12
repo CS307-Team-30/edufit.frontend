21f9fd7052b41a0577980d92fb7049c45a15a666
thomasibrahim23 <82554371+thomasibrahim23@users.noreply.github.com>
2023-10-12 18:26:48 -0400

    added component to page

--- pages/nutritionpage.tsx
+++ pages/nutritionpage.tsx
@@ -2,6 +2,7 @@ import "../src/styles/colors.css"
 import "../src/styles/globals.css"
 
 import ComponentsLayout from "@/app/components/layout";
+import NutritionInput from "@/app/components/NutritionInput";  
 
 // pages/about.tsx
 export default function NutritionPage() {
@@ -10,8 +11,9 @@ export default function NutritionPage() {
 
       <ComponentsLayout>
         <div className="md:mx-40 pt-20 px-12 mt-12 bg-white min-h-[500px]">
-          Nutrition
-        </div>
+        <h1 className="text-2xl mb-4">Nutrition Tracker</h1>  {/* Added a heading */}
+        <NutritionInput />  {/* Added the NutritionInput component */}
+      </div>
       </ComponentsLayout>
 
   );
