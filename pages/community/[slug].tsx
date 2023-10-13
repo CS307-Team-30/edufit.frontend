f83da80a6c96fa6edda3edbd57a4f47539342593
Arnob Kazi <shahirmuktasid22@gmail.com>
2023-10-12 21:23:27 -0400

    add framer motion stuff

--- pages/community/[slug].tsx
+++ pages/community/[slug].tsx
@@ -1,16 +1,20 @@
-import "../src/styles/colors.css"
-import "../src/styles/globals.css"
+import { useRouter } from "next/router";
+
+import "../../src/styles/colors.css"
+import "../../src/styles/globals.css"
 
 import ComponentsLayout from "@/app/components/layout";
 import Sidebar from "@/app/components/Sidebar";
 
-export default function CommunityPage() {
+export default function DynamicCommunityPage() {
+
+  const router = useRouter();
   return (
       <ComponentsLayout>
         <div className="flex flex-row justify-between">
           <Sidebar />
           <div className="md:mr-40 ml-20 pt-20 px-12 mt-12 bg-white w-full min-h-[500px] rounded-xl ">
-              Communities
+            {router.pathname}
           </div>
         </div>
       </ComponentsLayout>
--- src/app/components/PostBox.tsx
+++ src/app/components/PostBox.tsx
@@ -1,19 +1,34 @@
 import {motion} from 'framer-motion'
+import { useState } from 'react';
+import { BsFillArrowDownCircleFill } from 'react-icons/bs';
 
 export default function PostBox() {
- const handleSubmit = (event) => {
+ const handleSubmit = (event: any) => {
     event.preventDefault();
     // Handle form submission logic here
     console.log("Form submitted");
   };
 
-  return (
-    <motion.div>
 
+  const [toggle, setToggle] = useState(false)
+
+  return (
+    <motion.div
+      className='fixed bottom-0 right-1/4'
+      initial={{y:0}}
+      animate={toggle ? {y:250} : {y:0}}
+    >
       <form 
         onSubmit={handleSubmit}
-        className="min-h-[300px] w-[800px] text-pink-500 fixed bottom-0 right-1/4 bg-pink-50 shadow-lg z-30 rounded-lg px-4 py-4"
+        className="min-h-[300px] w-[800px] text-pink-500  bg-pink-50 shadow-lg z-30 rounded-lg px-4 py-4"
       >
+        <motion.button
+          initial={{rotate: 0, originX: 0, originY: 0}}
+          animate={toggle ? {rotate: 180} : {rotate:0}} 
+          onClick={() => setToggle(!toggle)} 
+          className='text-pink-500 left-1/2'>
+         <BsFillArrowDownCircleFill />
+        </motion.button>
         <h3>Post title</h3>
         <input 
           type="text" 
