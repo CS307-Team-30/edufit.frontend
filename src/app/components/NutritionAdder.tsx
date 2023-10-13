6c6284153b71d138f6a9c032b5c778b774c571f8
thomasibrahim23 <82554371+thomasibrahim23@users.noreply.github.com>
2023-10-12 20:03:59 -0400

    macro tracker

--- src/app/components/NutritionAdder.tsx
+++ src/app/components/NutritionAdder.tsx
@@ -1,64 +1,65 @@
 import * as React from 'react';
 
 const NutritionInput: React.FC = () => {
-    const [nutrition, setNutrition] = useState({
-        calories: '',
-        protein: '',
-        carbs: '',
-        fats: '',
-    });
+  const [nutrition, setNutrition] = React.useState({
+    calories: '',
+    protein: '',
+    carbs: '',
+    fats: '',
+  });
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setNutrition(prevState => ({
-            ...prevState,
-            [name]: value,
-        }));
-    };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setNutrition((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        console.log('Submitted Nutrition Data:', nutrition);
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
 
-    };
-
-    return (
-        <form onSubmit={handleSubmit} style={{ display: 'flex', marginBottom: '20px' }}>
-            <input
-                type="number"
-                name="calories"
-                placeholder="Calories"
-                value={nutrition.calories}
-                onChange={handleChange}
-                style={{ marginRight: '10px' }}
-            />
-            <input
-                type="number"
-                name="protein"
-                placeholder="Protein (g)"
-                value={nutrition.protein}
-                onChange={handleChange}
-                style={{ marginRight: '10px' }}
-            />
-            <input
-                type="number"
-                name="carbs"
-                placeholder="Carbs (g)"
-                value={nutrition.carbs}
-                onChange={handleChange}
-                style={{ marginRight: '10px' }}
-            />
-            <input
-                type="number"
-                name="fats"
-                placeholder="Fats (g)"
-                value={nutrition.fats}
-                onChange={handleChange}
-                style={{ marginRight: '10px' }}
-            />
-            <button type="submit">Submit</button>
-        </form>
-    );
+  return (
+    <form
+      onSubmit={handleSubmit}
+      style={{ display: 'flex', marginBottom: '20px' }}
+    >
+      <input
+        type='number'
+        name='calories'
+        placeholder='Calories'
+        value={nutrition.calories}
+        onChange={handleChange}
+        style={{ marginRight: '10px' }}
+      />
+      <input
+        type='number'
+        name='protein'
+        placeholder='Protein (g)'
+        value={nutrition.protein}
+        onChange={handleChange}
+        style={{ marginRight: '10px' }}
+      />
+      <input
+        type='number'
+        name='carbs'
+        placeholder='Carbs (g)'
+        value={nutrition.carbs}
+        onChange={handleChange}
+        style={{ marginRight: '10px' }}
+      />
+      <input
+        type='number'
+        name='fats'
+        placeholder='Fats (g)'
+        value={nutrition.fats}
+        onChange={handleChange}
+        style={{ marginRight: '10px' }}
+      />
+      <button type='submit'>Submit</button>
+    </form>
+  );
 };
 
 export default NutritionInput;
