357ea1c332c5e9f282d9dd143fa2502310b9e293
Arnob Kazi <shahirmuktasid22@gmail.com>
2023-10-12 20:54:40 -0400

    turkstra

--- src/app/components/PostBox.tsx
+++ src/app/components/PostBox.tsx
@@ -1,8 +1,41 @@
+import {motion} from 'framer-motion'
+
 export default function PostBox() {
+ const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission logic here
+    console.log("Form submitted");
+  };
+
   return (
-    <div className="h-[200px] w-[800px] text-pink-500  fixed bottom-0 right-1/4 bg-pink-50 shadow-lg z-30 rounded-lg px-4 py-4">
-      <h3>Post title</h3>
-      <p>Post description</p>
-    </div>
-  )
+    <motion.div>
+
+      <form 
+        onSubmit={handleSubmit}
+        className="min-h-[300px] w-[800px] text-pink-500 fixed bottom-0 right-1/4 bg-pink-50 shadow-lg z-30 rounded-lg px-4 py-4"
+      >
+        <h3>Post title</h3>
+        <input 
+          type="text" 
+          name="postTitle" 
+          placeholder="Enter post title" 
+          className="w-full p-2 mb-2 rounded border border-pink-300"
+        />
+        
+        <h3>Post description</h3>
+        <textarea 
+          name="postDescription" 
+          placeholder="Enter post description" 
+          className="w-full p-2 mb-2 rounded border border-pink-300"
+        ></textarea>
+        
+        <button 
+          type="submit" 
+          className="mt-2 bg-pink-500 text-white p-2 rounded"
+        >
+          Submit Post
+        </button>
+      </form>
+    </motion.div>
+  );
 }
