382ccb7e06b2134c9c8800cd79d9fc19f09d38b2
Arnob Kazi <shahirmuktasid22@gmail.com>
2023-10-12 21:56:20 -0400

    nutritionpage

--- pages/nutritionpage.tsx
+++ pages/nutritionpage.tsx
@@ -3,6 +3,7 @@ import '../src/styles/globals.css';
 
 import ComponentsLayout from '@/app/components/layout';
 import Nutrition from '@/app/components/Nutrition';
+import NutritionInput from '@/app/components/NutritionAdder';
 
 // pages/about.tsx
 export default function NutritionPage() {
@@ -13,7 +14,7 @@ export default function NutritionPage() {
           <Nutrition />
         </div>
         <div className='mt-12 min-h-[500px] bg-white px-12 pt-20 md:mx-40'>
-          <Nutrition />
+          <NutritionInput />
         </div>
       </div>
     </ComponentsLayout>
