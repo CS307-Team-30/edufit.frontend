145a26471f4778840390df0071846e8dd6c83879
thomasibrahim23 <82554371+thomasibrahim23@users.noreply.github.com>
2023-11-01 21:15:43 -0400

    health calculations

--- pages/nutritionpage.tsx
+++ pages/nutritionpage.tsx
@@ -1,6 +1,7 @@
 import '../src/styles/colors.css';
 import '../src/styles/globals.css';
 
+import HealthCalculator from '@/app/components/HealthComponent';
 import ComponentsLayout from '@/app/components/layout';
 import Nutrition from '@/app/components/Nutrition';
 import WorkoutComponent from '@/app/components/WorkoutComponent';
@@ -14,6 +15,9 @@ export default function NutritionPage() {
       </div>
       <div className='mt-12 min-h-[500px] bg-white px-12 pt-20 md:mx-40'>
         <WorkoutComponent /> 
+      </div>
+      <div className='mt-12 min-h-[500px] bg-white px-12 pt-20 md:mx-40'>
+        <HealthCalculator /> 
       </div>
     </ComponentsLayout>
       
