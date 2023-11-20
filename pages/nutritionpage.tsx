b8aa11b7699433af1831e73e8333af1b4b06c318
thomasibrahim23 <82554371+thomasibrahim23@users.noreply.github.com>
2023-11-19 20:51:50 -0600

    GoalOrMilestone

--- pages/nutritionpage.tsx
+++ pages/nutritionpage.tsx
@@ -5,6 +5,7 @@ import HealthCalculator from '@/app/components/HealthComponent';
 import Hydration from '@/app/components/Hydration';
 import ComponentsLayout from '@/app/components/layout';
 import Meals from '@/app/components/Meals';
+import GoalMilestoneInput from '@/app/components/MileStone';
 import WeightTracker from '@/app/components/Weight';
 import WorkoutComponent from '@/app/components/WorkoutComponent';
 
@@ -26,6 +27,9 @@ export default function NutritionPage() {
       </div>
       <div className='mt-12 min-h-[500px] bg-white px-12 pt-20 md:mx-40'>
         <WeightTracker />
+      </div>
+      <div className='mt-12 min-h-[500px] bg-white px-12 pt-20 md:mx-40'>
+        <GoalMilestoneInput />
       </div>
     </ComponentsLayout>
   );
