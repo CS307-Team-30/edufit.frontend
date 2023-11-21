6f826e3611180b0710efd40d7bb8ad80cba28e88
jhcheng01 <jhcheng@purdue.edu>
2023-11-21 09:49:25 -0600

    Merge from arnob

--- src/app/stores/UserStore.tsx
+++ src/app/stores/UserStore.tsx
@@ -1,32 +1,52 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
+import { Comment } from '@/types/Comment';
 import { Community } from '@/types/Community';
 import { Post } from '@/types/Post';
-import { initialProfileState, Profile } from '@/types/Profile';
 import { initialUserState, User } from '@/types/User';
+import { initialProfileState, Profile } from '@/types/Profile';
 
 type State = {
   user: User;
+  profile: Profile;
+  toggle: boolean;
   communities: Array<Community>;
   homepagePosts: Array<Post>;
-  profile: Profile;
-};
-
-type Action = {
+  commentsModal: Array<Comment>;
+  addCommentsModal: boolean;
+  setAddCommentsModal: (addCommentsModal: State['addCommentsModal']) => void;
+  setToggle: (toggle: State['toggle']) => void;
+  updateCommentsModal: (commentsModal: State['commentsModal']) => void;
   updateUser: (user: State['user']) => void;
   update: (communities: State['communities']) => void;
-  updatePosts: (homepagePosts: State['homepagePosts']) => void;
   updateProfile: (profile: State['profile']) => void;
+  updatePosts: (homepagePosts: State['homepagePosts']) => void;
 };
 
 // Create your store, which includes both state and (optionally) actions
-export const useGlobalStore = create<State & Action>((set) => ({
-  user: initialUserState,
-  communities: [],
-  homepagePosts: [],
-  profile: initialProfileState,
-  updateUser: (user) => set(() => ({ user: user })),
-  update: (communities) => set(() => ({ communities: communities })),
-  updatePosts: (homepagePosts) => set(() => ({ homepagePosts: homepagePosts })),
-  updateProfile: (profile) => set(() => ({ profile: profile })),
-}));
+export const useGlobalStore = create<State>()(
+  persist(
+    (set) => ({
+      user: initialUserState,
+      profile: initialProfileState,
+      toggle: false,
+      communities: [],
+      homepagePosts: [],
+      commentsModal: [],
+      addCommentsModal: false,
+      setToggle: (toggleState) => set(() => ({ toggle: toggleState })),
+      setAddCommentsModal: (addCommentsModal) =>
+        set(() => ({ addCommentsModal: addCommentsModal })),
+      updateCommentsModal: (commentsModal) =>
+        set(() => ({ commentsModal: commentsModal })),
+      updateUser: (user) => set(() => ({ user: user })),
+      update: (communities) => set(() => ({ communities: communities })),
+      updateProfile: (profile) =>
+        set(() => ({ profile: profile })),
+      updatePosts: (homepagePosts) =>
+        set(() => ({ homepagePosts: homepagePosts }))
+    }),
+    { name: 'global', getStorage: () => localStorage }
+  )
+);
